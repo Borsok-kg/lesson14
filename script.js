cd46cc08a29b505f47a75a702cb700300570bce4
Azat Tagaev <braza@yandex.ru>
2021-03-04 22:02:58 +0600

    14 урок сложный

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 'use strict'
 
-const DomElement = function(selector, height, width, bg, fontSize, text) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.text = text;
-};
+document.addEventListener("DOMContentLoaded", function() {
 
-DomElement.prototype.add = function() {
-    let elem;
-    if (this.selector.includes('.')) {
-        elem = document.createElement('div');
-        elem.classList.add(this.selector.slice(1));
-        elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: #${this.bg}; font-size: ${this.fontSize}px;`;
-        elem.innerHTML = this.text;
-        document.body.append(elem);
-    } 
-    if (this.selector.includes('#')) {
-        elem = document.createElement('div');
-        elem.id = this.selector.slice(1);
-        elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: #${this.bg}; font-size: ${this.fontSize}px;`;
-        elem.innerHTML = this.text;
-        document.body.append(elem);
-    }
-};
+    const DomElement = function(selector, height, width, bg, fontSize, text) {
+        this.selector = selector;
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.text = text;
+    };
+    
+    DomElement.prototype.add = function() {
+        let elem;
+        if (this.selector.includes('.')) {
+            elem = document.createElement('div');
+            elem.classList.add(this.selector.slice(1));
+            elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: #${this.bg}; font-size: ${this.fontSize}px;`;
+            elem.innerHTML = this.text;
+            document.body.append(elem);
+        } 
+        if (this.selector.includes('#')) {
+            elem = document.createElement('div');
+            elem.id = this.selector.slice(1);
+            elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: #${this.bg}; font-size: ${this.fontSize}px;`;
+            elem.innerHTML = this.text;
+            document.body.append(elem);
+        }
+    };
 
-let classBlock = new DomElement('.element', '100', '100', '808080', '20', 'По классу');
-let classBlock2 = new DomElement('#element', '100', '100', '808080', '20', 'По id');
+    DomElement.prototype.move = function() {
+        let elem = document.querySelector('div');
+        elem.style.cssText += `position: absolute;`;
+    };
+    
+    let classBlock = new DomElement('.element', '100', '100', '808080', '20', 'По классу');
+    // let classBlock2 = new DomElement('#element', '100', '100', '808080', '20', 'По id');
+    
+    document.addEventListener('keydown', function(event) {
+        let elem = document.querySelector('div'),
+            up = elem.offsetTop,
+            left = elem.offsetLeft;
 
-classBlock.add();
-classBlock2.add();
+        if (event.key === 'ArrowUp') {
+            elem.style.top = up - 10 + 'px';
+        } else if (event.key === 'ArrowLeft') {
+            elem.style.left = left - 10 + 'px';
+        } else if (event.key === 'ArrowRight') {
+            elem.style.left = left + 10 + 'px';
+        } else if (event.key === 'ArrowDown') {
+            elem.style.top = up + 10 + 'px';
+        }
+    });
+
+    classBlock.add();
+    // classBlock2.add();
+    classBlock.move();
+
+});
+
